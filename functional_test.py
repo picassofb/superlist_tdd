3fce6b2ed716500fd35603791697154d645d4585
Ed <picassofb@hotmail.com>
2016-09-18 12:01:36 -0500

    UnitTest

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,19 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+	
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+        self.browser.implicicitly_wait(3)
 
-assert 'Django' in browser.title
+    def tearDown():	
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_larger(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
